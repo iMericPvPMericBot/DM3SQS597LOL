0e320cb129477340e98b5b3f74219f4e4bf5e3e4
iMericPvPMericBot <44123161+iMericPvPMericBot@users.noreply.github.com>
2018-10-13 23:23:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,7 +55,7 @@ client.on('message', message => {
 
 
 
-lient.on("message", message => {
+client.on("message", message => {
       if (message.content === ".ping") {
       const embed = new Discord.RichEmbed()
   .setColor("RANDOM")
