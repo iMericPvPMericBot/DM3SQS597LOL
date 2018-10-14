89e754f068e40335a5fa1441ca62768b7958ac2f
iMericPvPMericBot <44123161+iMericPvPMericBot@users.noreply.github.com>
2018-10-14 07:28:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -245,15 +245,4 @@ message.channel.sendEmbed(cat);
 });
 
 
-client.on('message', msg =>{
-    let message=msg;
-    if(message.content.startsWith(".bc")){
-        var args = message.content.split(' ').slice(1).join(' ');
-    msg.guild.members.forEach(m=>{
-        m.send(args.replace(/[user]/g,m)).catch();
-    if(message.attachments.first()){
-m.sendFile(message.attachments.first().url).catch();
-    }
-    })    ;
-    }
-});
+
